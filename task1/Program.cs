f42cee2499e5f33baaeda6bc4fbf6abe67f1d01d
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-21 19:35:05 +0300

    задача23

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// таблица кубов от 1 до N
+int num = new Random().Next(1, 12);
+
+System.Console.WriteLine($"num = {num}");
+
+for(int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
+
