e96b5aa09df603e999199c7ee9c6c7e060b0d830
Евгений <Grekos_89@mail.ru>
2023-01-22 15:44:13 +0300

    задача19

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// является ли число полиндромом
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = 0;
+int revers = 0;
+int num3 = num;
+
+while( num3 > 0 )
+{
+    num2 = num3 % 10;
+    revers = revers * 10 + num2;
+    num3 = num3 / 10;
+}
+if( num == revers)
+{
+    Console.WriteLine("полиндром");
+}
+else
+{
+    Console.WriteLine("Не полиндром");
+}
